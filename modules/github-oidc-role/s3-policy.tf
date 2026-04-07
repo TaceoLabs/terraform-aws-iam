216e2b5a5f0202ba56be984be5e0913494405258
Anyplace44 <guggi@taceo.io>
2026-04-07 14:41:03 +0200

    added s3 bucket policy

---
+++ modules/github-oidc-role/s3-policy.tf
@@ -0,0 +1,22 @@
+data "aws_iam_policy_document" "github_s3_policy" {
+  #TODO Reduce permissions to only necessary resources
+  statement {
+    effect    = "Allow"
+    actions   = ["s3:PutObject"]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "github_s3_policy" {
+  count       = var.s3_policy.enabled ? 1 : 0
+  name        = "github-s3-policy"
+  description = "Policy for GitHub Actions to create and destroy resources for s3's"
+  policy      = data.aws_iam_policy_document.github_s3_policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "github_s3_policy" {
+  count      = var.s3_policy.enabled ? 1 : 0
+  role       = aws_iam_role.this.name
+  policy_arn = aws_iam_policy.github_s3_policy[0].arn
+}
+
